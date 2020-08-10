498db574f8fdc00fe76ebc99eb64cec60de75807
moon <moon@DESKTOP-SDG1QTD>
2020-08-10 20:15:46 +0900

    test

---
+++ chap01/src/sec03/exam01/day1/Hello.java
@@ -0,0 +1,17 @@
+package sec03.exam01.day1;
+//클래스 선언부
+//public은 공개클래스 Hello는 클래스 이름, 클래스 이름은 소스 파일명과 동일해야 하며, 대소문자도 일치해야 한다.
+
+public class Hello {//class block
+	//method 선언부, main은 method의 이름
+	public static void main(String[] args) {	//method block
+		
+		System.out.println("Hello, java");	//run 하면 method block 내부에 있는 명령문 실행, 실행문 종료시 ;(세미콜론)을 붙여줘야 한다.
+	}//end method block
+
+}//end class block
+// //부터 라인 끝까지 주석으로 처리
+/*
+	이 사이에 있는 내용 모두 주석으로 처리
+*/
+/** 이 사이에 있는 내용 모두 주석으로 처리 API 도큐먼트 생성시 사용 */
---
+++ chap01/src/sec03/exam02/day2/RunStatementExample.java
@@ -0,0 +1,13 @@
+package sec03.exam02.day2;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		int x = 1;						//변수 정수(int) x를 선언하고 1을 저장
+		int y = 2;						//변수 정수(int) y를 선언하고 2를 저장	
+		int result = x + y;				//변수 정수(int) result를 선언하고 x와 y를 더한 값을 저장
+		System.out.println(result);		//Console창에 출력하는 method 호출
+
+	}
+
+}
